1e056601da27fb70dd1bb4967fcebcaef5d6a696
Kelly Severson <kseverson26@student.cvtc.edu>
2024-10-02 12:45:27 -0500

    Add test for Vaild and Invalid inputs

--- test/MainTest.java
+++ test/MainTest.java
@@ -60,5 +60,68 @@ class MainTest {
     );
   }
 
-  // TODO: Create your test(s) below. /////////////////////////////////////////
+  //Test Valid input. Should add to the bigram count.
+    @Test
+    void testAddBigramWithValidInput() throws SQLException {
+        try (Connection db = Main.createConnection()) {
+            int word1 = Main.getId(db, "cat");
+            int word2 = Main.getId(db, "the");
+
+            int initialBigrams = getBigramCount(db);
+            System.out.println("Initial count: " + initialBigrams);
+
+            Main.addBigram(db, word1, word2);
+
+            int newBigrams = getBigramCount(db);
+            System.out.println("Count after valid input: " + newBigrams);
+
+            assertEquals(initialBigrams + 1, newBigrams,
+                    String.format("The bigram count should increase by 1."));
+        }
+    }
+
+    // Test negative ID. Should not change the bigram count.
+    @Test
+    void testAddInvalidInputNegative() throws SQLException {
+        try (Connection db = Main.createConnection()) {
+            int initialBigrams = getBigramCount(db);
+            System.out.println("Initial count: " + initialBigrams);
+
+            // Test invalid input with negative IDs
+            System.out.println("Test invalid input (-1, -1)");
+            assertDoesNotThrow(() -> Main.addBigram(db, -1, -1),
+                    "Negative ID should not throw exception");
+
+            int currentBigrams = getBigramCount(db);
+
+            assertEquals(initialBigrams, currentBigrams, "The bigram count should remain unchanged for invalid input.");
+        }
+    }
+
+    // Test 0 ID. Should not change the bigram count.
+    @Test
+    void testAddInvalidInputZero() throws SQLException {
+        try (Connection db = Main.createConnection()) {
+            int initialBigrams = getBigramCount(db);
+            System.out.println("Initial count: " + initialBigrams);
+
+            // Test invalid input with zero ID
+            assertDoesNotThrow(() -> Main.addBigram(db, 0, 0),
+                    "Zero ID should not throw exception");
+
+            int currentBigrams = getBigramCount(db);
+            System.out.println("Count after input 0: " + currentBigrams);
+
+            assertEquals(initialBigrams, currentBigrams,
+                    "The bigram count should remain unchanged for invalid input.");
+        }
+    }
+
+    // Find size of database
+    private int getBigramCount(Connection conn) throws SQLException {
+        Statement command = conn.createStatement();
+        ResultSet rows = command.executeQuery("SELECT COUNT(*) AS count FROM bigrams");
+        rows.next();
+        return rows.getInt(1);
+    }
 }
